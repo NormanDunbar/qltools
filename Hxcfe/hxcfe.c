e3c7bde7cb7834a1ad8920f05b5850c5620f33bb
Graeme Gregory <graeme@xora.org.uk>
2014-03-21 19:58:22 +0000

    hxcfe/linux: add a getch implementation
    I got a bit carried away and removed the old one.
    
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

--- Hxcfe/hxcfe.c
+++ Hxcfe/hxcfe.c
@@ -150,3 +150,28 @@ void ZeroSomeSectors(int fd, short d)
 		WriteQLSector (fd, buf, i);
 	}
 }
+
+#ifndef __MINGW32__
+#include <termios.h>
+int getch(void) {
+	char buf=0;
+	struct termios old={0};
+	fflush(stdout);
+	if(tcgetattr(0, &old)<0)
+		perror("tcsetattr()");
+	old.c_lflag&=~ICANON;
+	old.c_lflag&=~ECHO;
+	old.c_cc[VMIN]=1;
+	old.c_cc[VTIME]=0;
+	if(tcsetattr(0, TCSANOW, &old)<0)
+		perror("tcsetattr ICANON");
+	if(read(0,&buf,1)<0)
+		perror("read()");
+	old.c_lflag|=ICANON;
+	old.c_lflag|=ECHO;
+	if(tcsetattr(0, TCSADRAIN, &old)<0)
+		perror ("tcsetattr ~ICANON");
+	printf("%c\n",buf);
+	return buf;
+}
+#endif
--- Unix/linux.c
+++ Unix/linux.c
@@ -58,7 +58,6 @@ time_t GetTimeZone(void)
     return  -60 * tz.tz_minuteswest;
 }
 
-/* ARGSUSED */
 void ZeroSomeSectors(int fd, short d)
 {
     int i;
@@ -71,3 +70,28 @@ void ZeroSomeSectors(int fd, short d)
 	write(fd, buf, 512);
     }
 }
+
+#ifndef __MINGW32__
+#include <termios.h>
+int getch(void) {
+	char buf=0;
+	struct termios old={0};
+	fflush(stdout);
+	if(tcgetattr(0, &old)<0)
+		perror("tcsetattr()");
+	old.c_lflag&=~ICANON;
+	old.c_lflag&=~ECHO;
+	old.c_cc[VMIN]=1;
+	old.c_cc[VTIME]=0;
+	if(tcsetattr(0, TCSANOW, &old)<0)
+		perror("tcsetattr ICANON");
+	if(read(0,&buf,1)<0)
+		perror("read()");
+	old.c_lflag|=ICANON;
+	old.c_lflag|=ECHO;
+	if(tcsetattr(0, TCSADRAIN, &old)<0)
+		perror ("tcsetattr ~ICANON");
+	printf("%c\n",buf);
+	return buf;
+}
+#endif
